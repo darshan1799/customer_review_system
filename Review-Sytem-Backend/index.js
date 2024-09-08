241bd798d32d23ef07b04a6955c2ffff9a0016a5
darshan sutariya <pateldarshan1322005@gmail.com>
2024-09-08 15:57:30 +0530

    sovle bug related cors origin

--- Review-Sytem-Backend/index.js
+++ Review-Sytem-Backend/index.js
@@ -11,7 +11,7 @@ app.use(express.json());
 app.use(CookieParser());
 
 const corsOptions = {
-  origin: "http://localhost:5173",
+  origin: "https://customer-review-omega.vercel.app",
   credentials: true,
 };
 app.use(cors(corsOptions));
